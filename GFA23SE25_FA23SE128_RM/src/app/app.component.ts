cc5dbab3dd24723431b0e99237d9fc118e3ae3c7
nguyenanhquan0 <quan251299@gmail.com>
2023-09-08 11:57:45 +0700

    set up

---
+++ GFA23SE25_FA23SE128_RM/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'GFA23SE25_FA23SE128_RM';
+}

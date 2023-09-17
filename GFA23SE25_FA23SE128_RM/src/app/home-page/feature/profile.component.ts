bfc534050bf5fb25cec12a394c759632b43e1874
nguyenanhquan0 <quan251299@gmail.com>
2023-09-17 15:11:59 +0700

    profile

--- GFA23SE25_FA23SE128_RM/src/app/home-page/feature/profile.component.ts
+++ GFA23SE25_FA23SE128_RM/src/app/home-page/feature/profile.component.ts
@@ -1,15 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, NzGridModule, NzUploadModule],
+  imports: [CommonModule, NzGridModule, NzUploadModule, NzImageModule, NzMenuModule],
   template: `
     <div nz-row nzJustify="start">
-      <div nz-col nzSpan="6">col-4</div>
+      <div nz-col nzSpan="6">
+        <div class=" tw-text-center">
+          <img
+            class="tw-rounded-full"
+            nz-image
+            width="200px"
+            height="200px"
+            nzSrc="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            alt=""
+          />
+          <h1>Yasuo</h1>
+          <h5>Shop owner</h5>
+        </div>
+        <ul nz-menu nzTheme="light" nzMode="inline">
+          <li nz-menu-item>
+            <span>Thông tin tài khoản</span>
+          </li>
+          <li nz-menu-item>
+            <span>123</span>
+          </li>
+          <li nz-menu-item>
+            <span class="tw-text-red-500">Đăng xuất</span>
+          </li>
+        </ul>
+      </div>
       <div nz-col nzSpan="18">col-4</div>
     </div>
   `,

5317c10fb458b41241563906b1e8c494d8678ca2
nguyenanhquan0 <quan251299@gmail.com>
2023-09-22 13:54:51 +0700

    update profile

--- GFA23SE25_FA23SE128_RM/src/app/home-page/feature/profile.component.ts
+++ GFA23SE25_FA23SE128_RM/src/app/home-page/feature/profile.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, NzGridModule, NzUploadModule, NzImageModule, NzMenuModule],
+  imports: [CommonModule, NzGridModule, NzUploadModule, NzImageModule, NzMenuModule, NzInputModule, NzButtonModule],
   template: `
     <div nz-row nzJustify="start">
       <div nz-col nzSpan="6">
@@ -16,8 +18,8 @@ import { NzUploadModule } from 'ng-zorro-antd/upload';
           <img
             class="tw-rounded-full"
             nz-image
-            width="200px"
-            height="200px"
+            width="50%"
+            height="50%"
             nzSrc="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
             alt=""
           />
@@ -25,7 +27,7 @@ import { NzUploadModule } from 'ng-zorro-antd/upload';
           <h5>Shop owner</h5>
         </div>
         <ul nz-menu nzTheme="light" nzMode="inline">
-          <li nz-menu-item>
+          <li nz-menu-item nzSelected>
             <span>Thông tin tài khoản</span>
           </li>
           <li nz-menu-item>
@@ -36,7 +38,32 @@ import { NzUploadModule } from 'ng-zorro-antd/upload';
           </li>
         </ul>
       </div>
-      <div nz-col nzSpan="18">col-4</div>
+      <div nz-col nzSpan="18">
+        <div nz-row>
+          <div nz-col nzSpan="12">
+            <h3 class="tw-ml-5">Họ</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>\
+            <h3 class="tw-ml-5">Email</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>
+            <h3 class="tw-ml-5">Giới tính</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>
+          </div>
+          <div nz-col nzSpan="12">
+            <h3 class="tw-ml-5">Tên</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>\
+            <h3 class="tw-ml-5">Số điện thoại</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>
+            <h3 class="tw-ml-5">Ngày sinh</h3>
+            <input nz-input disabled class="tw-w-[80%] tw-ml-5"/>
+          </div>
+        </div>
+        <h3 class="tw-ml-5">Địa Chỉ</h3>
+        <input nz-input disabled class="tw-w-[90%] tw-ml-5"/> \<br>
+        <div class="tw-mt-7 tw-text-center">
+        <button nz-button nzType="primary" nzDanger>Hủy</button>
+        <button nz-button class="tw-ml-3" nzType="primary">Cập Nhật</button>
+      </div>
+      </div>
     </div>
   `,
   styles: [],

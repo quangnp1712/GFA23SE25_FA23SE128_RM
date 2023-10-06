cb201287938ba3cf53d4f7bc161b2ed27292f3de
minh.tq <minh.tq@ots.vn>
2023-10-06 07:55:37 +0700

    Fix bug

--- be-core/src/main/java/com/realman/becore/dto/shop_owner/ShopOwnerMapper.java
+++ be-core/src/main/java/com/realman/becore/dto/shop_owner/ShopOwnerMapper.java
@@ -1,12 +1,11 @@
 package com.realman.becore.dto.shop_owner;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 import com.realman.becore.repository.database.shop_owner.ShopOwnerEntity;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShopOwnerMapper {
     ShopOwner toDto(ShopOwnerEntity entity);
 
